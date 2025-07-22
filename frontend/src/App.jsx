6cb1b5813004a87d1a32a1c8ad49772144142a47
opekk <maciek09874@gmail.com>
2025-07-22 17:55:50 +0200

    troche zmian, przejscie na Vite w froncie i podlaczenie tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-xl font-bold underline text-center">
+          Hello world!
+        </h1>
+      </div>
+  </>
+    
+  )
+}
+
+export default App;
