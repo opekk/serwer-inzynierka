3c6c7a90d2a6f1f77f6b9e18a6a36e446b3fb19d
opekk <maciek09874@gmail.com>
2025-07-23 11:51:58 +0200

    stworzenie compomentu Navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        inter: ['inter', 'sans-serif']
+      },
+      colors: {
+        'custom-gray': '#3A404E'
+      }
+    },
   },
   plugins: [],
 };
