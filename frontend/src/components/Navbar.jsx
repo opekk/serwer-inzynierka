a2f9cf3382392cd4e9e4c230ba3d61e14ad3cff7
opekk <maciek09874@gmail.com>
2025-07-25 16:57:05 +0200

    stworzenie componentu Footer, dodanie contentu na strone testowa.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 export default function Navbar() {
+<<<<<<< HEAD
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const [isAnimating, setIsAnimating] = useState(false)
 
@@ -172,6 +173,24 @@ export default function Navbar() {
                                 </Link>
                             </div>
                         </div>
+=======
+    return(
+         <nav class="bg-white font-inter border border-gray-200 rounded shadow-xl">
+            <div class="max-w-8xl mx-auto px-3 sm:px-5 lg:px-8 ml-20">
+                <div class="flex items-center justify-between h-16 w-full">
+                    <div class="text-slate-900 text-xl font-bold mr-8">
+                        <a href="">AuctionHub</a></div>
+                    <div class="flex flex-1 space-x-4">
+                        <a href="#" class="text-slate-500 px-4 py-2 hover:underline hover:underline-offset-6 hover:text-blue-200">Strona Główna</a>
+                        <a href="#" class="text-slate-500 px-4 py-2 hover:underline hover:underline-offset-6 hover:text-blue-200">Aukcje na zywo</a>
+                        <a href="#" class="text-slate-500 px-4 py-2 hover:underline hover:underline-offset-6 hover:text-blue-200">Kategorie</a>
+                        <a href="#" class="text-slate-500 px-4 py-2 hover:underline hover:underline-offset-6 hover:text-blue-200">Domy Aukcyjne</a>
+                        <a href="#" class="text-slate-500 px-4 py-2 hover:underline hover:underline-offset-6 hover:text-blue-200">Jak to działa?</a>
+                    </div>
+                    <div>
+                        <button class="px-4 py-2 text-slate-900 bg-gray-100 hover:bg-blue-400 rounded-xl mr-3">Zaloguj się</button>
+                        <button class="px-5 py-2 text-white bg-indigo-700 hover:bg-blue-400 rounded-xl">Zarejestruj się</button>
+>>>>>>> c820f05 (stworzenie componentu Footer, dodanie contentu na strone testowa.)
                     </div>
                 </div>
             </div>
--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 
 export default function TestPage() {
   return (
+<<<<<<< HEAD
     <div className="flex flex-col min-h-screen">
       <Navbar />
        <div className="mt-12 bg-gray-100 p-8 rounded-lg">
@@ -13,10 +14,22 @@ export default function TestPage() {
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <p className="text-gray-700">
+=======
+    <div class="flex flex-col min-h-screen">
+      <Navbar />
+       <div class="mt-12 bg-gray-100 p-8 rounded-lg">
+            <h2 class="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
+            <p class="text-gray-700 mb-6">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p class="text-gray-700">
+>>>>>>> c820f05 (stworzenie componentu Footer, dodanie contentu na strone testowa.)
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
               totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
             </p>
           </div>
+<<<<<<< HEAD
            <div className="mt-12 bg-gray-100 p-8 rounded-lg">
             <h2 className="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
             <p className="text-gray-700 mb-6">
@@ -24,10 +37,20 @@ export default function TestPage() {
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <p className="text-gray-700">
+=======
+           <div class="mt-12 bg-gray-100 p-8 rounded-lg">
+            <h2 class="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
+            <p class="text-gray-700 mb-6">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p class="text-gray-700">
+>>>>>>> c820f05 (stworzenie componentu Footer, dodanie contentu na strone testowa.)
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
               totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
             </p>
           </div>
+<<<<<<< HEAD
            <div className="mt-12 bg-gray-100 p-8 rounded-lg">
             <h2 className="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
             <p className="text-gray-700 mb-6">
@@ -35,10 +58,20 @@ export default function TestPage() {
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <p className="text-gray-700">
+=======
+           <div class="mt-12 bg-gray-100 p-8 rounded-lg">
+            <h2 class="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
+            <p class="text-gray-700 mb-6">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p class="text-gray-700">
+>>>>>>> c820f05 (stworzenie componentu Footer, dodanie contentu na strone testowa.)
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
               totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
             </p>
           </div>
+<<<<<<< HEAD
            <div className="mt-12 bg-gray-100 p-8 rounded-lg mb-12">
             <h2 className="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
             <p className="text-gray-700 mb-6">
@@ -46,6 +79,15 @@ export default function TestPage() {
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <p className="text-gray-700">
+=======
+           <div class="mt-12 bg-gray-100 p-8 rounded-lg mb-12">
+            <h2 class="text-2xl font-bold mb-6 text-center">Sekcja Dodatkowa</h2>
+            <p class="text-gray-700 mb-6">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p class="text-gray-700">
+>>>>>>> c820f05 (stworzenie componentu Footer, dodanie contentu na strone testowa.)
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
               totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
             </p>
