cc1198316a5efac989d8967dc51de2dbfde5b514
Patryk Nowacki <p.nowacki7@wp.pl>
2025-10-12 23:07:10 +0200

    Skonczona strona glowna, dodany plik Auctions.jsx polaczenie go z navbarem, strona glowna

--- frontend/src/pages/Auctions.jsx
+++ frontend/src/pages/Auctions.jsx
@@ -4,9 +4,15 @@ import Footer from '../components/Footer'
 
 export default function Auctions() {
   return ( 
+<<<<<<< HEAD
     <div className="text-black">
       <Navbar />
       <div className="max-w-7xl min-h-screen">
+=======
+    <div class="text-black">
+      <Navbar />
+      <div class="max-w-7xl min-h-screen">
+>>>>>>> f0453a9 (Skonczona strona glowna, dodany plik Auctions.jsx polaczenie go z navbarem, strona glowna)
       <h1>Tu jeszcze nic nie ma</h1>
       </div>
       <Footer />
