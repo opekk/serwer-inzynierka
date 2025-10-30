947df8262290c2b004246ffb46203b14291ffd6e
F1STjd <Konrad03Nowak@gmail.com>
2025-10-30 14:44:54 +0100

    New better routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,39 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles/index.css'
 import TestPage from './pages/TestPage.jsx'
 import MainPage from './pages/MainPage.jsx'
 import Auctions from './pages/Auctions.jsx'
 import AuctionView from './pages/AuctionView.jsx'
 import Viewer3D from './pages/Viewer3D.jsx'
-
-function AppRouter() {
-  const path = window.location.pathname
-  if (path === '/main' || path === '/' || path === '') {
-    return <MainPage />
-  }
-  if (path === '/test') {
-    return <TestPage />
-  }
-
-  if (path === '/auctions') {
-    return <Auctions />
-  } 
-
-  if (path === '/auctionview') {
-    return <AuctionView />
-  }
-
-  if (path === '/viewer3d' || path === '/3d') {
-    return <Viewer3D />
-  }
-
-  // Fallback to MainPage for unknown routes
-  return <MainPage />
-}
+import { WebGPUCanvasProvider } from './components/WebGPUCanvasProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <AppRouter />
+    <BrowserRouter>
+  <WebGPUCanvasProvider>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/auctions" element={<Auctions />} />
+          <Route path="/auctionview" element={<AuctionView />} />
+          <Route path="/viewer3d" element={<Viewer3D />} />
+          <Route path="/3d" element={<Viewer3D />} />
+          <Route path="*" element={<MainPage />} />
+        </Routes>
+      </WebGPUCanvasProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
